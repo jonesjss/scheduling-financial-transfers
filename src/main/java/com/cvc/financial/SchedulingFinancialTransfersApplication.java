26e89e1d0b59cf0139c297754e71b60180fdfbd2
Jones Santos <jones.jssantos@gmail.com>
2022-02-09 17:37:02 -0300

    Initial commit: initial dependencies added

---
+++ src/main/java/com/cvc/financial/SchedulingFinancialTransfersApplication.java
@@ -0,0 +1,13 @@
+package com.cvc.financial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingFinancialTransfersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingFinancialTransfersApplication.class, args);
+	}
+
+}
