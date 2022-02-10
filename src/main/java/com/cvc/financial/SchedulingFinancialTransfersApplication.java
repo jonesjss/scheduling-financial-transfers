0de80d86e5344c184d7ab04d8c6cf044af628ca1
Jones Santos <jones.jssantos@gmail.com>
2022-02-09 23:11:25 -0300

    added annotations to improve performance of repositories and entities

--- src/main/java/com/cvc/financial/SchedulingFinancialTransfersApplication.java
+++ src/main/java/com/cvc/financial/SchedulingFinancialTransfersApplication.java
@@ -1,8 +1,14 @@
 package com.cvc.financial;
 
+import com.cvc.financial.domain.model.User;
+import com.cvc.financial.domain.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan(basePackageClasses = User.class)
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 @SpringBootApplication
 public class SchedulingFinancialTransfersApplication {
 
