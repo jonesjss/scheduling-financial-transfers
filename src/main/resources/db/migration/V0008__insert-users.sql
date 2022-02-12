5990d83365eff80cbab418399ef3e24f1e196060
Jones Santos <jones.jssantos@gmail.com>
2022-02-11 23:20:36 -0300

    added migrations for user and account

---
+++ src/main/resources/db/migration/V0008__insert-users.sql
@@ -0,0 +1,6 @@
+INSERT
+INTO
+  users
+  (name, creation)
+VALUES
+  ('USER', NOW());
---
+++ src/main/resources/db/migration/V0009__insert-accounts.sql
@@ -0,0 +1,6 @@
+INSERT
+INTO
+  accounts
+  (agency, account_number, user_id)
+VALUES
+  ('0002', '987654', 2);
